4d3a03e69585a57b75e01158427347b7f65375ef
Omar gb <neo_e_xe@hotmail.com>
2021-02-19 03:38:33 -0600

    Agregar archivos de proyecto.

---
+++ MusicQueryService/IQueryMusic.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace MusicQueryService
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de interfaz "IQueryMusic" en el código y en el archivo de configuración a la vez.
+    [ServiceContract]
+    public interface IQueryMusic
+    {
+        [OperationContract]
+        [WebGet(BodyStyle = WebMessageBodyStyle.Bare,
+            RequestFormat = WebMessageFormat.Json,
+            ResponseFormat = WebMessageFormat.Json,
+            UriTemplate = "artistByGenre/{Genere}")]
+        IEnumerable<Artist> ArtistByGenere(string Genere);
+
+        [OperationContract]
+        [WebGet(BodyStyle = WebMessageBodyStyle.Bare,
+            RequestFormat = WebMessageFormat.Json,
+            ResponseFormat = WebMessageFormat.Json,
+            UriTemplate = "albumByArtist/{Artist}")]
+        IEnumerable<Album> AlbumByArtist(string Artist);
+
+        [OperationContract]
+        [WebGet(BodyStyle = WebMessageBodyStyle.Bare,
+            RequestFormat = WebMessageFormat.Json,
+            ResponseFormat = WebMessageFormat.Json,
+            UriTemplate = "songsByAlbum/{Album}")]
+        IEnumerable<Tracks> SongsByAlbum(string Album);
+        [OperationContract]
+        [WebGet(BodyStyle = WebMessageBodyStyle.Bare,
+            RequestFormat = WebMessageFormat.Json,
+            ResponseFormat = WebMessageFormat.Json,
+            UriTemplate = "artistBySong/{song}")]
+        IEnumerable<Artist> ArtistBySong(string song);
+
+
+        [OperationContract]
+        [WebGet(BodyStyle = WebMessageBodyStyle.Bare,
+             RequestFormat = WebMessageFormat.Json,
+             ResponseFormat = WebMessageFormat.Json,
+             UriTemplate = "allGenre")]
+        IEnumerable<Genre> AllGenre();
+
+    }
+}
---
+++ MusicQueryService/QueryMusic.svc.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+
+namespace MusicQueryService
+{   
+    //clase que contine  la logica del servicio 
+    public class QueryMusic : IQueryMusic
+    {
+        WebScrapingResources _ws = new WebScrapingResources();
+        DataManager _Dm = new DataManager();
+
+        //Artista por genero
+        public IEnumerable<Artist> ArtistByGenere(string Genre)
+        {
+            //envia cómo parámetro los criterios de busqueda para el web scraping
+            var dc = _ws.utiliti("https://us.napster.com/search/artist?query=", Genre, ".name");
+            List<Artist> lstArtist = new List<Artist>();
+            foreach (var l in dc)
+            {
+                Artist a = new Artist
+                {
+                    NameArtist = l.ToString()
+                };
+                lstArtist.Add(a);
+            }
+            _Dm.InsertArtist(lstArtist, Genre);
+            //retorna la lista de objetos
+            return lstArtist;
+        }
+
+        //Album por artista
+        public IEnumerable<Album> AlbumByArtist(string Artist)
+        {
+            var dc = _ws.utiliti("https://us.napster.com/search?query=", Artist, ".album-link");
+            List<Album> lstAlbum = new List<Album>();
+            foreach (var l in dc)
+            {
+                Album a = new Album
+                {
+                    NameAlbum = l.ToString()
+                };
+                lstAlbum.Add(a);
+            }
+            _Dm.InsertAlbum(lstAlbum, Artist);
+            return lstAlbum;
+        }
+
+        //Canciones por album
+        public IEnumerable<Tracks> SongsByAlbum(string Album)
+        {
+            var dc = _ws.utiliti("https://us.napster.com/search/track?query=", Album, ".name");
+            List<Tracks> lstSongs = new List<Tracks>();
+            foreach (var l in dc)
+            {
+                Tracks song = new Tracks
+                {
+                    nameSong = l.ToString()
+                };
+                lstSongs.Add(song);
+            }
+            _Dm.InsertSongs(lstSongs, Album);
+            return lstSongs;
+        }
+
+        //artistas por canción
+        public IEnumerable<Artist> ArtistBySong(string song)
+        {
+            var dc = _ws.utiliti("https://us.napster.com/search?query=", song, ".artist-link");
+            List<Artist> lstArtist = new List<Artist>();
+            foreach (var l in dc)
+            {
+                Artist artist = new Artist
+                {
+                    NameArtist = l.ToString()
+                };
+                lstArtist.Add(artist);
+            }
+            _Dm.InsertSoundtrack(lstArtist, song);
+            return lstArtist;
+        }
+        //Todos los generos
+        public IEnumerable<Genre> AllGenre()
+        {            
+            return _ws.AllGenre();
+        }
+    
+    }
+}

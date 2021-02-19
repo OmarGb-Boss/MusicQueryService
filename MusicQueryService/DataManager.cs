ee62ee73973910745eb2379220ce1ff66404db8c
Omar gb <neo_e_xe@hotmail.com>
2021-02-19 03:38:33 -0600

    Agregar archivos de proyecto.

---
+++ MusicQueryService/DataManager.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MusicQueryService.DataSorange;
+
+namespace MusicQueryService
+{
+    //Está clase genera la inserción de datos, como historial de búsqueda
+    //sus métodos reciben como parámetro listas de información obtenidas del web scraping
+    public class DataManager
+    {
+
+        dbInfoMusicEntities1 Context = new dbInfoMusicEntities1();
+        DateTime thisDay = DateTime.Today;
+        //Artista por genero
+        public bool InsertArtist(List<Artist> artist, string genero)
+        {
+            bool result = false;
+            if (artist != null)
+            {
+                try
+                {
+                    foreach (var a in artist)
+                    {
+                        Artista artistObject = new Artista
+                        {
+                            NombreArtista = a.NameArtist,
+                            GeneroMusical = genero,
+                            FechaInsert = Convert.ToDateTime(thisDay.ToShortDateString())
+                        };
+                        Context.Entry(artistObject).State = System.Data.Entity.EntityState.Added;
+                        Context.SaveChanges();
+                    }
+
+                    result = true;
+                }
+                catch
+                {
+                    result = false;
+                }
+            }
+            else
+            {
+                result = false;
+            }
+
+            return result;
+        }
+
+        //Album por artista
+        public bool InsertAlbum(List<Album> album, string artista)
+        {
+            bool result = false;
+            if (album != null)
+            {
+                try
+                {
+                    foreach (var a in album)
+                    {
+                        AlbumMusical albumMusical = new AlbumMusical
+                        {
+                            NombreAlbum = a.NameAlbum,
+                            NombreArtista = artista,
+                            FechaInsert = Convert.ToDateTime(thisDay.ToShortDateString())
+                        };
+                        Context.Entry(albumMusical).State = System.Data.Entity.EntityState.Added;
+                        Context.SaveChanges();
+                    }
+                    result = true;
+                }
+                catch(Exception e)
+                {
+                    result = false;
+                }
+
+            }
+            else
+            {
+                result = false;
+            }
+            return result;
+        }
+
+        //Canciones por album
+        public bool InsertSongs(List<Tracks> track, string album)
+        {
+
+            bool result = false;
+            if (album != null)
+            {
+                try
+                {
+                    foreach (var t in track)
+                    {
+                        Songs songs = new Songs
+                        {
+                            NombreSong = t.nameSong,
+                            NombreAlbum = album,
+                            FechaInsert = Convert.ToDateTime(thisDay.ToShortDateString())
+                        };
+                        Context.Entry(songs).State = System.Data.Entity.EntityState.Added;
+                        Context.SaveChanges();
+                    }
+                    result = true;
+                }
+                catch
+                {
+                    result = false;
+                }
+            }
+            else
+            {
+                result = false;
+            }
+
+            return result;
+        }
+
+        //artistas por cancion
+        public bool InsertSoundtrack(List<Artist> artista, string song)
+        {
+            
+            bool result = false;
+
+            if (artista != null)
+            {
+                try
+                {
+                    foreach (var a in artista)
+                    {
+                        SoundtrackArtist songs = new SoundtrackArtist
+                        {
+                            NameArtist = a.NameArtist,
+                            Soundtrack = song,
+                            FechaInsert = Convert.ToDateTime(thisDay.ToShortDateString())
+                        };
+                        Context.Entry(songs).State = System.Data.Entity.EntityState.Added;
+                        Context.SaveChanges();
+                    }
+
+                    result = true;
+                }
+                catch
+                {
+                    result = false;
+                }
+            }
+            else
+            {
+                result = false;
+            }             
+            return result;
+        }
+
+    }
+}
---
+++ MusicQueryService/WebScrapingResources.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ScrapySharp.Extensions;
+using HtmlAgilityPack;
+
+namespace MusicQueryService
+{
+    public class WebScrapingResources
+    {
+        //para obtener los datos de la web se utilizó la libreria ScrapySharp.Extensions y HtmlAgilityPack
+        //genera los resultados de la búsqueda  por web scraping 
+        public List<string> utiliti(string Query, string concept, string classe)
+        {
+            HtmlWeb OWEB = new HtmlWeb();
+            HtmlDocument Doc = OWEB.Load(string.Concat(Query, concept));
+            string ClearNode = string.Empty;
+            List<string> Elements = new List<string>();
+            foreach (var gen in Doc.DocumentNode.CssSelect(classe))
+            {
+                switch (Query)
+                {
+                    case "https://us.napster.com/search/artist?query=":
+                        var node = gen.CssSelect("a").First();
+                        ClearNode = node.InnerText;
+                        ClearNode = ClearNode.Replace("\n", "").Replace("\r", "");
+                        Elements.Add(ClearNode);
+                        break;
+
+                    default:
+                        ClearNode = gen.InnerText;
+                        ClearNode = ClearNode.Replace("\n", "").Replace("\r", "");
+                        Elements.Add(ClearNode);
+                        break;
+                }
+            }
+            return Elements;
+        }
+
+        //Muestra todos los generos y subgeneros musicales de la pagina
+        public IEnumerable<Genre> AllGenre()
+        {
+            HtmlWeb OWEB = new HtmlWeb();
+            HtmlDocument Doc = OWEB.Load("https://us.napster.com/music");
+            List<Genre> lstGeneres = new List<Genre>();
+            foreach (var gen in Doc.DocumentNode.CssSelect(".genre-name-text"))
+            {
+                Genre genereObject = new Genre();
+                genereObject.NameGenere = gen.InnerHtml;
+                genereObject.NameGenere = genereObject.NameGenere.Replace("\n", "").Replace("\r", "");
+                Doc = OWEB.Load(string.Concat("https://us.napster.com/genre/", genereObject.NameGenere));
+                
+                genereObject.NameGenere = genereObject.NameGenere;
+                genereObject.lstSubGeneres = new List<SubGenre>();
+                foreach (var subGen in Doc.DocumentNode.CssSelect(".tag-button"))
+                {
+                    genereObject.lstSubGeneres.Add(new SubGenre{NombreSubGenero = subGen.InnerHtml });
+                }
+                lstGeneres.Add(genereObject);
+            }
+            return lstGeneres;
+        }
+
+    }
+
+    //entidades para poblar la información
+    public class Artist
+    {
+        public string NameArtist { get; set; }
+    }
+
+    public class Album
+    {
+        public string NameAlbum { get; set; }
+    }
+
+    public class Tracks 
+    {
+        public string nameSong { get; set; }
+    }
+
+
+    public class SubGenre
+    {
+        public string NombreSubGenero { get; set; }
+    }
+
+    public class Genre
+    {
+        public string NameGenere { get; set; }        
+        public List<SubGenre> lstSubGeneres;
+    }
+
+
+}
